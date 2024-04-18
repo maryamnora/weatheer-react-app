45b9b0df41cc14535d455443c1d8acb8cea59748
maryamnora <m.ahmadi021@gmail.com>
2024-04-18 14:35:08 -0400

    added GitHub link

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather App
+     <h1>Weather App</h1> 
+     <footer>
+      This site is coded by Maryam and is {""}
+      <a href='https://github.com/maryamnora/weatheer-react-app'target='_blank' rel="noreferrer" >open-sourced </a>    </footer>
     </div>
   );
 }
