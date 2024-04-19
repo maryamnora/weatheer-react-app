c1438f3b63e8fc9c12a5e6134b522fe41cd7b4a6
maryamnora <m.ahmadi021@gmail.com>
2024-04-19 13:27:02 -0400

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 
 import './App.css';
 
+
 export default function App() {
   return (
     <div className="App">
-     <h1>Weather App</h1> 
-     <footer>
+     <div className="container">   <h1>Weather App</h1> 
+      <footer>
       This site is coded by Maryam and is {""}
-      <a href='https://github.com/maryamnora/weatheer-react-app'target='_blank' rel="noreferrer" >open-sourced </a>    </footer>
+      <a href='https://github.com/maryamnora/weatheer-react-app'target='_blank' rel="noreferrer" >
+        open-sourced on GitHub </a>
+        <button className="btn btn-primary">hello</button>
+      </footer>
+     </div>
     </div>
   );
 }
