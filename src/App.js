9de2a3429abf22113e02001a034008ee0ede5275
maryamnora <m.ahmadi021@gmail.com>
2024-05-03 14:53:38 -0400

    corrected footer

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ export default function App() {
      <div className="container">  
      <Weather defaultCity="New York"/>
       <footer className="footer mt-1">
-      This site is coded by Maryam and is open-sourced on {""}
+       coded by Maryam and is open-sourced on {""}
       <a href='https://github.com/maryamnora/weatheer-react-app'target='_blank' rel="noreferrer" >
          GitHub </a> and hosted on <a href="https://weather-app-maryam.netlify.app/"target='_blank' rel="noreferrer">Netlify</a>
         
